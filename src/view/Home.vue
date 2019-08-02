ad5161b079660565c073dfd3c76f9b4a94f6dd30
namkang321 <namkang_chutisilp@hotmail.com>
2019-08-02 23:35:45 +0700

    change Home layout

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -15,20 +15,40 @@
             to raise the bar on the possible. Quite simply, it is the oldest,
             largest, and most prestigious programming contest in the world.
         </p>
-      <div class="">
-        <v-btn
-          href="https://icpc.baylor.edu/regionals/abouticpc"
-          target="_blank"
-          depressed
-          color="#6b0000"
-          dark
-          >More</v-btn
-        >
-      </div>
+        <div  align='right' style='width: 100%'>
+            <v-btn
+                href="https://icpc.baylor.edu/regionals/abouticpc"
+                target="_blank"
+                depressed
+                color="#6b0000"
+                right
+                dark
+            >
+                More
+            </v-btn>
+        </div>
     </div>
-    <div class="py-5" id="content-chula-eng">
-      <div class="container py-5">
-        <div class="pb-5" id="content-cu-eng">
+    <div id="content-chula-eng">
+      <div class="container">
+        <div align="right" style="width: 100%">
+            <h1
+                style="font-size: 5em; color: #6b0000; font-family: Playfair Display, Times, serif;"
+            >
+                About Host
+            </h1>
+            <v-flex md8>
+                <p style="font-size: 18px;">
+                Department of Computer Engineering, Faculty of Engineering,
+                Chulalongkorn University is the first computer school in Thailand. The
+                origin came from the Computer Science Unit of Graduate School which
+                was the computer services unit inside Chulalongkorn University, and
+                also educating computer science in the level of certificate and master
+                degree. Later, the Computer Science Unit was transformed into the
+                Department of Computer Engineering.
+                </p>
+            </v-flex>
+        </div>
+        <div class="pt-5" id="content-cu-eng">
           <img
             :src="require(`@/assets/cu-eng-logo.png`)"
             alt="Chula Engineering"
@@ -36,17 +56,6 @@
             style="display: block; margin-left: auto; margin-right: auto;"
           />
         </div>
-        <v-flex d-flex md6>
-            <p style="font-size: 18px;">
-            Department of Computer Engineering, Faculty of Engineering,
-            Chulalongkorn University is the first computer school in Thailand. The
-            origin came from the Computer Science Unit of Graduate School which
-            was the computer services unit inside Chulalongkorn University, and
-            also educating computer science in the level of certificate and master
-            degree. Later, the Computer Science Unit was transformed into the
-            Department of Computer Engineering.
-            </p>
-        </v-flex>
       </div>
     </div>
   </div>
