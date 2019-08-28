a8732bb53e88d14aa22f24e4e956ed715fef61bf
namkang321 <namkang_chutisilp@hotmail.com>
2019-08-28 23:33:21 +0700

    change Rules

--- src/view/Rules.vue
+++ src/view/Rules.vue
@@ -1,245 +1,90 @@
 <template>
   <div class="px-5 mx-5 my-5">
-    <h1>ICPC Regional Rules for 2019 Regionals</h1>
+    <h1>กฎกติกาการแข่งขัน</h1>
     <p>
-      Adapted for the local site, these rules conform to the official ICPC Regional Rules. (<a
-        href="http://icpc.baylor.edu/regionals/rules"
-        target="_blank"
-        >http://icpc.baylor.edu/regionals/rules</a
-      >) and ACM-ICPC 2016 Asia Official Regional Rules (<a
-        href="http://blog.sina.com.cn/s/blog_b946da100102wyh6.html"
-        target="_blank"
-        >http://blog.sina.com.cn/s/blog_b946da100102wyh6.html</a
-      >)
+      กฎและการตัดสินที่ใช้ในการแข่งขัน อ้างอิงจากกฎการแข่งขันหลักของ ACM-ICPC ในระดับ World Final รวมไปถึงระดับ Regional
+      ของภูมิภาคต่าง ๆ โดยคณะกรรมการฯ ได้ปรับปรุงบางส่วนให้เข้ากับสภาพการแข่งขันในพื้นที่ปัจจุบัน
+      ผู้เข้าร่วมแข่งขันทุกทีมและผู้ควบคุมทีม รวมถึงผู้ช่วยผู้ควบคุมทีม ควรอ่านทำความเข้าใจและปฏิบัติตามอย่างเคร่งครัด
+      เพื่อความเสมอภาคและความยุติธรรมในการแข่งขัน
     </p>
     <br /><br />
-    <h2 class="display-1">Mission</h2>
+    <h2 class="display-1">ข้อกำหนดของทีม</h2>
     <p>
-      The ACM International Collegiate Programming Contest (ICPC) provides college students with opportunities to
-      interact with students from other universities and to sharpen and demonstrate their problem-solving, programming,
-      and teamwork skills. The contest provides a platform for ACM, industry, and academia to encourage and focus public
-      attention on the next generation of computing professionals as they pursue excellence.
+      ทีมต้องประกอบด้วยโค้ช 1 คน (อาจมีผู้ติดตามได้) และผู้เข้าแข่งขัน 3 คน ซึ่งมาจากสถาบันเดียวกัน
+      หากไม่เป็นไปตามข้อกำหนดนี้ จะไม่ได้รับการจัดอันดับ รางวัล และสิทธิ์ในการผ่านเข้าสู่รอบถัดไป
+      แต่สามารถเข้าร่วมการแข่งขันได้โดยได้รับอนุญาตจากผู้จัดการแข่งขัน
     </p>
+    <br /><br />
+    <h2 class="display-1">ลักษณะการแข่งขัน</h2>
     <p>
-      The ACM-ICPC Asia Regional Contests invite Asian students to meet, to establish friendships, and to promote fair
-      competition in programming and in application use of algorithms and theory.
+      แข่งขันเขียนโปรแกรมแก้โจทย์ปัญหา (โจทย์อธิบายเป็นภาษาอังกฤษ) โดยใช้ภาษา C, C++ หรือ Java เป็นภาษาโปรแกรม
+      เวลาแข่งขัน 3 ชั่วโมง ติดต่อกัน ทีมเข้าแข่งขันเลือกทำโจทย์ข้อใดก่อนก็ได้ แต่ละข้อใช้ภาษาโปรแกรมอะไรก็ได้ (C, C++,
+      Java) ทีมที่เข้าแข่งขัน มีสมาชิก 3 คน เป็นนิสิต/นักศึกษาจากสถาบันเดียวกัน
     </p>
+    <br /><br />
+    <h2 class="display-1">ข้อกำหนดในห้องแข่งขัน</h2>
     <br />
-    <h2 class="display-1">Team Composition</h2>
+    <ol>
+      <li>
+        อนุญาตให้นำหนังสือ อาทิ Text Book, Dictionary, Magazine เข้าได้ทีมละไม่เกิน 3 เล่ม (สามารถเป็นแบบถ่ายสำเนาได้)
+        เอกสารอื่น ๆ ในรูปแบบกระดาษไม่เกิน 100 หน้า รวมถึงอุปกรณ์เครื่องเขียนต่างๆ โดยให้นำมาฝากไว้ ก่อนการแข่งจริง
+        และไม่อนุญาตให้นำเข้าห้องเพิ่ม
+      </li>
+      <li>ไม่อนุญาตให้นำสัมภาระหรือสิ่งของใดๆ เข้าไปในห้องแข่ง</li>
+      <li>
+        ไม่อนุญาตให้ใช้อุปกรณ์สื่อสารหรืออุปกรณ์อิเล็กทรอนิกส์ทุกชนิด อาทิ โทรศัพท์, เครื่องคิดเลข, เครื่องเล่น MP3,
+        Flash Drive, CD, iPod เป็นต้น
+      </li>
+      <li>
+        ผู้เข้าแข่งขันต้องใช้เครื่องคอมพิวเตอร์และอุปกรณ์ที่ทางผู้จัดการแข่งขันจัดเตรียมไว้ให้เท่านั้น
+        ไม่อนุญาตให้นำอุปกรณ์มาเอง
+      </li>
+      <li>
+        ไม่อนุญาตให้ผู้เข้าแข่งขันติดต่อสื่อสารกับบุคคลอื่นใด นอกเหนือจากสมาชิกภายในทีมตนเอง
+        และคณะกรรมการจัดการแข่งขันหรือนิสิตช่วยงานในระหว่างการแข่งขัน
+      </li>
+      <li>
+        ในระหว่างการแข่งขัน สมาชิกในทีมอาจขออนุญาตกรรมการประจำห้องไปห้องน้ำหรือผ่อนคลาย ภายนอกห้องแข่งขัน
+        ในขอบเขตที่กำหนดไว้ และมีคณะกรรมการคอยเฝ้าติดตาม แต่ละครั้งไม่เกิน 15 นาที
+      </li>
+      <li>ไม่ควรส่งเสียงดังเกินควรหรือทำการอื่นใด ที่เป็นการรบกวนทีมอื่น ๆ</li>
+      <li>ไม่อนุญาต ให้ใช้ Internet หรือ พยายามติดต่อใช้ Internet ด้วยวิธีการใด ๆ</li>
+    </ol>
+    <br /><br />
+    <h2 class="display-1">ระหว่างการแข่งขัน</h2>
     <p>
-      A representative of the sponsoring institution of higher education, typically a faculty member, must serve as or
-      designate the team coach. The coach certifies the eligibility of contestants and serves as the official
-      point-of-contact with the team prior to and during contest activities. A team may only have one coach and the
-      coach cannot also be a contestant.
+      การส่งโปรแกรมเพื่อรับการตรวจแต่ละครั้ง จะเรียกว่า Run
+      โดยเมื่อส่งไปแล้วระบบจะตรวจและแจ้งว่าโปรแกรมดังกล่าวสามารถทำงานได้ถูกต้องหรือไม่ (Yes/No)
+      ผู้เข้าแข่งขันทุกทีมจำเป็นต้องเข้าสู่ระบบ (Login) ผ่านโปรแกรม PC2
+      ตลอดระยะเวลาการแข่งขันด้วยชื่อผู้ใช้งานที่แจกให้แต่ละทีมเท่านั้น
+      หากพบความพยายามในการเข้าสู่ระบบด้วยชื่อผู้ใช้งานอื่นจะถูกพิจารณาตัดสิทธิ์จากการแข่งขัน
     </p>
     <p>
-      The coach must fully register teams in the ICPC Registration System within the time set by the regional rules
-      which, for all Regional Contests, is no later than October 23. A team is not eligible to compete in the regional
-      contest until the regional contest director has accepted the team in the web registration system. Teams failing to
-      comply with any of these requirements will be ruled ineligible to compete.
+      หากผู้เข้าแข่งขันพบความผิดปกติในโจทย์แข่งขัน ให้สอบถามผ่านทางระบบ Clarification Request ของ PC2
     </p>
     <p>
-      Each team consists of three contestants who are eligible to compete in the ICPC World Finals as described under
-      Advancing to the World Finals in the ICPC Regional Rules. The team's contestants must satisfy the following
-      eligibility rules.
+      ในระหว่างการแข่งขัน ผู้เข้าแข่งขันสามารถเข้าชม Live Scoreboard
+      แจ้งอันดับของทีมได้ผ่านขั้นตอนที่จะระบุในห้องแข่งขัน และจะมีการแสดง Live Scoreboard
+      ไปยังห้องรับรองผู้ควบคุมทีมด้วยเช่นเดียวกัน ซึ่งจะทำการปิดระบบ Live Scoreboard ก่อนหมดเวลการแข่งขัน 45 นาที
     </p>
-    <br />
-    <h3 class="headline">Basic Requirements</h3>
-    <ul>
-      <li>
-        A student must be willing and able to compete in the World Finals. In particular, a student who is attending the
-        team’s institution on a visa must have permission to travel to the return destination after the World Finals.
-      </li>
-      <li>
-        A student must be enrolled in a degree program at the team’s institution with at least a half-time load. This
-        rule is not to be construed as disqualifying co-op students, exchange students, students serving internships, or
-        extramural students.
-      </li>
-      <li>A student may compete for only one institution during a contest year.</li>
-      <li>A student who has competed in two World Finals is NOT eligible to compete.</li>
-      <li>A student who has competed in Regional Contests for five years is NOT eligible to compete.</li>
-      <li>A student may compete in at most two Asia Site Contests, home or not-home administrative sub-region.</li>
-    </ul>
+    <p>
+      ผู้เข้าแข่งขันจะได้รับผลการตรวจ (Judge’s Response - Yes/No) ตามปกติตลอดระยะเวลาการแข่งขัน
+    </p>
     <br /><br />
-    <h3 class="headline">Period of Eligibility</h3>
-    <ul>
-      <li>
-        A student who meets the Basic Requirements and FIRST began post-secondary studies in 2012 or later is eligible
-        to compete.
-      </li>
-      <li>A student who meets the Basic Requirements and was born in 1993 or later is eligible to compete.</li>
-    </ul>
-    <br /><br />
-    <br /><br />
-    <h2 class="display-1">Conduct of the contest</h2>
-    <br /><br />
-    <h3 class="headline">Pre-Contest</h3>
-    <ol>
-      <li>
-        A calculator and any electronic devices or storages (e.g. mobile phone, CD, DVD, USB flash drive, MP3 player,
-        iPod, iPad, electronic dictionary, etc) are NOT permitted. However, each contestant is allowed to bring one
-        paper dictionary and some personal and stationery items (pen, pencil, ruler, dice, etc) into a contest room.
-        Blank papers will be provided at each workstation.
-      </li>
-      <li>
-        A team can bring up to 3 identical copies of Team Reference Document which is defined as follows.
-        <ul>
-          <li>
-            <b>Team Reference Document</b> consists of letter-size or A4-size papers containing notes and/or printed
-            pages (single-sided or double-sided). The total number of pages of the document must not exceed 25 pages.
-            <i style="text-decoration: underline;"
-              >All pages must be numbered and the team name and university must appear in the header of every page.</i
-            >
-            Documents must be bought in a folder or stapled together.
-          </li>
-          <li>
-            The copies of <b>Team Reference Document and other permitted items</b> will be reviewed by an inspector and
-            <b>left at team’s workstation</b> on the first day (practice session) to be used in the contest session on
-            the next day.
-          </li>
-        </ul>
-      </li>
-      <li>
-        All teams will use the same equipment and software environment as provided by the organizer. No substitution of
-        keyboards or other hardware will be allowed for the competition. The provided keyboard has a standard QWERTY
-        keyboard layout. We suggest that the contestants acquire a similar keyboard for practice.
-      </li>
-      <li>
-        All contestants must wear the contestant T-Shirt and the ID badge (provided on-site) throughout the contest.
-        Only 3 contestants per team are allowed to get into the contest room.
-      </li>
-    </ol>
-    <br /><br />
-    <h3 class="headline">In-Contest</h3>
-    <ol>
-      <li>
-        At least 10 problems will be posed. So far as possible, problems avoid dependence on detailed knowledge of a
-        particular applications area or particular contest language. Of these problems at least two will be solvable by
-        a second-year computer science student and the rest will determine the winner.
-      </li>
-      <li>
-        Solutions to contest problems submitted for judging are called runs. Each run is judged as accepted (Yes) or
-        rejected (No) by a judge, and the team is notified of the results within a few minutes after the reception of
-        the run.
-      </li>
-      <li>
-        A team should stay logged-in to the PC2 system throughout the contest time with their own account. Logging-in as
-        another team is considered as a cheat.
-      </li>
-      <li>
-        Teams should inform the volunteers if they do not get a reply from the judges within 15 minutes after
-        submission. Volunteers will report that to the judges. Teams should also notify the volunteers immediately if
-        any problem concerning PC2 system (e.g. disconnection, forgotten password, program freezing) occurs. Complaints
-        regarding these topics made after the contest is finished may not be considered.
-      </li>
-      <li>
-        Notification of rejected or accepted runs to the contestant teams will not be suspended for the last one hour of
-        the contest time but to keep the final result secret the displayed scoreboard will not be updated for last 1
-        hour.
-      </li>
-      <li>
-        A contestant may submit a clarification request to judges. If the judges agree that an ambiguity error exists, a
-        clarification will be issued to all contestants. A trivial request may not be responded.
-      </li>
-      <li>
-        Contestants are not to converse with anyone except members of their team and personnel designated by the
-        organizing committee. Systems support staff may advise contestants on system-related problems such as explaining
-        system error messages.
-      </li>
-      <li>
-        A team may be disqualified by the Contest Committee for any cheat or attempt of cheating. A team may also be
-        disqualified for any activity that jeopardizes the contest such as dislodging extension cords, unauthorized
-        modification of contest materials, or distracting behavior.
-      </li>
-      <li>
-        While the contest is scheduled for a particular time length (5 hours), the judges have the authority to alter
-        the length of the contest in the event of unforeseen difficulties. Should the contest duration be altered, every
-        attempt will be made to notify contestants in a timely and uniform manner.
-      </li>
-      <li>
-        With the help of volunteers, the contestants can have a printout of their program listings for debugging
-        purposes. Each team has printing quota of 30 pages.
-      </li>
-    </ol>
-    <br /><br />
-    <h3 class="headline">Post-Contest</h3>
-    <ol>
-      <li>
-        Any complaints about the contest must be made to the Contest Committee and Judges within 30 minutes after the
-        contest. After the result has been announced, further complaints must be made according to the Regional Rules
-        (<a href="http://icpc.baylor.edu/regionals/rules" target="_blank">http://icpc.baylor.edu/regionals/rules</a>)
-      </li>
-      <li>
-        In consultation with the judges, the Contest Director determines the winners of the contest. The contest
-        director and judges are empowered to adjust for or adjudicate unforeseen events and conditions. Their decisions
-        are final.
-      </li>
-      <li>The official contest result and ranking will be announced after the contest in the closing ceremony.</li>
-    </ol>
-    <br /><br />
-    <h2 class="display-1">Scoring and Ranking</h2>
-    <ol>
-      <li>
-        A problem is solved when it is accepted by the judges. The judges are solely responsible for accepting or
-        rejecting submitted runs.
-      </li>
-      <li>
-        The total time of a team is the sum of the time consumed for each problem solved. The time consumed for a solved
-        problem is the time elapsed from the beginning of the contest to the submittal of the accepted run plus 20
-        penalty minutes for every rejected run for that problem regardless of submittal time. There is no time consumed
-        for a problem that is not solved.
-      </li>
-      <li>
-        Teams are ranked according to the most problems solved. For the purposes of awards, or in determining
-        qualifier(s) for the World Finals, teams who solve the same number of problems are ranked by least total time.
-        In the case of equal total time, the less time of the latest problem solved of each team (not including penalty
-        time for that problem) will determine the higher ranked team. If still equal, the time of next-latest problem(s)
-        will be considered.
-      </li>
-      <li>
-        During the contest the live scoreboard (containing the number of problems solved of each team and the current
-        ranking) will be shown to the people outside the contest rooms. The contestants and coaches can also have an
-        access to the live scoreboard during the contest. However, the scoreboard will be frozen at about 1 hour before
-        the end of the contest in order to keep the final results secret.
-      </li>
-      <li>
-        Each submission, called <i>run</i> will get one of these responses from the judges:
-        <ul>
-          <li>
-            Yes – Accepted. <br />
-            The program produced the correct output within running time limit without any error. This program is
-            accepted by the judges and the problem is considered as solved.
-          </li>
-          <li>
-            No – Wrong Answer. <br />
-            The program did not produce the correct output.
-          </li>
-          <li>
-            No – Runtime Error. <br />
-            The program crashed (runtime error) when processing the secret test data.
-          </li>
-          <li>
-            No – Time Limit Exceeded. <br />
-            The program took more than the time limit to produce output and terminate correctly.
-          </li>
-          <li>
-            No – Compilation Error. <br />
-            The submitted source code cannot be compiled.
-          </li>
-        </ul>
-        <p>
-          All four <i>No</i> responses are considered rejection and will increase your time by 20 minutes if you solve
-          the problem.
-        </p>
-        <p>
-          There may be multiple test cases for some problems. For such problems, the test cases will be judged
-          sequentially. The runs is accepted if it is accepted by all test cases. The first rejection will be the run
-          result, regardless of the result of other test cases. For example, Problem A has 4 test cases. Your submission
-          got Wrong Answer on 2nd test case, and Runtime Error on 3rd test case. In this case, the judge response will
-          be Wrong Answer.
-        </p>
-      </li>
-    </ol>
+    <h2 class="display-1">การตัดสิน</h2>
+    <p>
+      การเรียงอันดับของทีม นับจากจำนวนโจทย์ที่ทำได้ (Problem Solved) เป็นหลัก ถ้ามีทีมที่ทำได้จำนวนโจทย์เท่ากัน
+      ให้เรียงตามเวลา (Total Time) จากน้อยไปมาก Total Time คือ ผลรวมของเวลา (จำนวนนาที นับจากจุดเริ่มต้นการแข่งขัน)
+      ที่ใช้ทำโจทย์ในแต่ละข้อ โดยจะพิจารณาเฉพาะข้อที่ทำได้ถูกต้อง
+      โดยเลือกเอาเวลาที่ใช้แก้ปัญหาโจทย์ได้เป็นครั้งแรกของข้อนั้นๆ (First Accepted Run)
+      รวมกับเวลาที่เกิดจากการส่งโปรแกรมคำตอบ (Run) ที่ทำงานไม่ถูกต้องของข้อนั้นๆ จะถูกเพิ่มเวลา (Penalty) เป็น 20
+      นาทีต่อครั้ง เวลาของโจทย์ของข้อที่ยังไม่ถูกต้องจะไม่นำไปคิดในเวลารวม หากมีตั้งแต่สองทีมขึ้นไปที่มีจำนวนข้อ Problem
+      Solved และ Total Time เท่ากันพอดี ให้พิจารณาจากเวลาของข้อสุดท้ายที่ทีมนั้นๆ ทำได้ โดยไม่คำนึงถึงเวลาที่เกิดจาก
+      Penalty ของข้อนั้น ใครทำได้ก่อนเป็นผู้ชนะ หากเท่ากันอีกจึงพิจารณาข้อก่อนหน้าตามลำดับ
+      หากพบเจตนาในการทุจริตของทีมใดๆ คณะกรรมการมีสิทธิ์ในการตัดสิทธิ์การแข่งขัน หากมีข้อร้องเรียนใดๆ เกี่ยวกับการแข่งขัน
+      ต้องดำเนินการให้แล้วเสร็จภายใน 30 นาทีหลังจากสิ้นสุดการแข่งขัน ในกรณีที่เกิดเหตุการณ์ไม่ปกติในการแข่งขัน
+      คณะกรรมการจัดการแข่งขันมีสิทธิ์ในการตัดสินเป็นกรณีไป และการตัดสินของคณะกรรมการจัดการแข่งขันถือเป็นสิ้นสุด
+    </p>
   </div>
 </template>
 
