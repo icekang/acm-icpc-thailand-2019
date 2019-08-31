fa014873476fa9c9adf835605dfdd7a6bc4f9374
namkang321 <namkang_chutisilp@hotmail.com>
2019-08-31 21:11:27 +0700

    adjust layout of Contest page

--- src/view/Contest.vue
+++ src/view/Contest.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="px-5 mx-5 my-5">
     <h1>Contest Information</h1>
-    <h2 class="display-1">Time</h2>
-    <p>19 October 2019</p>
-    <p>8:30 - 12:00 (Register 8:30 - 9:00)</p>
-    <h2 class="display-1">Location</h2>
+    <h2 class="display-1 pb-2">Time</h2>
+    <p>
+      19 October 2019
+      <br />
+      8:30 - 12:00 (Register 8:30 - 9:00)
+    </p>
+    <h2 class="display-1 pb-2">Location</h2>
     <p>
       Computer Center, 2nd Floor Building 3, Faculty of Engineering, Chulalongkorn University, 254 Phayathai Road,
       Wangmai, Pathumwan, Bangkok, 10330
-    </p>
-    <p>
+      <br />
       ศูนย์คอมพิวเตอร์ อาคาร 3 ชั้น 2 คณะวิศวกรรมศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย 254 ถนนพญาไท แขวงวังใหม่ เขตปทุมวัน กทม.
       10330
     </p>
